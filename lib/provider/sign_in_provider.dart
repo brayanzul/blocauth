e5dd5fd737fd0b8273fc3daea1c9622c3727777e
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-04-10 15:14:25 -0500

    Flutter BlocAuth project and today I worked: Login Screen: Here add the twitter botton, Sign In Provider Adding the logic to login with twitter

--- lib/provider/sign_in_provider.dart
+++ lib/provider/sign_in_provider.dart
@@ -7,6 +7,9 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:twitter_login/twitter_login.dart';
+
+import '../utils/config.dart';
 
 class SignInProvider extends ChangeNotifier {
 
@@ -14,6 +17,11 @@ class SignInProvider extends ChangeNotifier {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   final FacebookAuth facebookAuth = FacebookAuth.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn();
+  final twitterLogin = TwitterLogin(
+    apiKey: Config.apikey_twitter,
+    apiSecretKey: Config.secretkey_twitter,
+    redirectURI: "socialauth://"
+  );
 
   bool _isSignedIn = false;
   bool get isSignedIn => _isSignedIn;
@@ -83,6 +91,49 @@ class SignInProvider extends ChangeNotifier {
         _uid = userDetails.uid;
         notifyListeners();
 
+      } on FirebaseAuthException catch (e) {
+        switch (e.code) {
+          case "account-exists-with-different-credential":
+            _errorCode = 
+              "You already have an account with us. Use correct provider";
+            _hasError = true;
+            notifyListeners();
+            break;
+
+          case "null":
+            _errorCode = "Some unexpected error while trying to sign in";
+            _hasError = true;
+            notifyListeners();
+            break;
+            
+          default:
+            _errorCode = e.toString();
+            _hasError = true;
+            notifyListeners();
+        }
+      }
+    } else {
+      _hasError = true;
+      notifyListeners();
+    }
+  }
+
+  // sign in with twitter
+  Future signInWithTwitter() async {
+    final authResult = await twitterLogin.loginV2();
+    if(authResult.status == TwitterLoginStatus.loggedIn) {
+      try {
+        final credential = TwitterAuthProvider.credential(accessToken: authResult.authToken!, secret: authResult.authTokenSecret!);
+        await firebaseAuth.signInWithCredential(credential);
+
+        final userDetails = authResult.user;
+        // save all the data
+        _name = userDetails!.name;
+        _email = firebaseAuth.currentUser!.email;
+        _imageUrl = userDetails.thumbnailImage;
+        _provider = "TWITTER";
+        _hasError = false;
+        notifyListeners();
       } on FirebaseAuthException catch (e) {
         switch (e.code) {
           case "account-exists-with-different-credential":
